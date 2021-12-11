a455c0095ae4e43dffb596c3d1faff2e9e086af3
Bane Radosavljević <r_bane@hotmail.com>
2021-12-11 15:26:41 +0100

    Episode 3

---
+++ tests/Feature/ContactFormTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Http\Livewire\ContactForm;
+use App\Mail\ContactFormMailable;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Mail;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class ContactFormTest extends TestCase
+{
+    /** @test */
+    public function main_page_contains_contact_form_livewire_component()
+    {
+        $this->get('/')
+            ->assertSeeLivewire('contact-form');
+    }
+
+    /** @test */
+    public function contact_form_sends_out_an_email()
+    {
+        Mail::fake();
+
+        Livewire::test(ContactForm::class)
+            ->set('name', 'Bane')    
+            ->set('email', 'bane@gmail.com')    
+            ->set('phone', '0645633456')    
+            ->set('message', 'This is my message.')
+            ->call('submitForm')
+            ->assertSee('We received your message successfully and will get back to you shortly!');     
+
+        Mail::assertSent(function(ContactFormMailable $mail){
+            $mail->build();
+
+            return $mail->hasTo('bane@hotmail.com') && 
+                $mail->hasFrom('bane@gmail.com') &&
+                $mail->subject === 'Contact Form Submission';
+        });
+    } 
+
+    /** @test */
+    public function contact_form_name_field_is_required()
+    {
+        Livewire::test(ContactForm::class)
+            // ->set('name', 'Bane')    
+            ->set('email', 'bane@gmail.com')    
+            ->set('phone', '0645633456')    
+            ->set('message', 'This is my message.')
+            ->call('submitForm')
+            ->assertHasErrors(['name' => 'required']);
+    }
+
+    /** @test */
+    public function contact_form_message_field_has_min_characters()
+    {
+        Livewire::test(ContactForm::class)
+            ->set('message', 'This')
+            ->call('submitForm')
+            ->assertHasErrors(['message' => 'min']);
+    }
+}
